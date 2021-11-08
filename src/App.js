1e7d178570c2ffc92e67ff86f0a2318fa18c3ff3
Rio0628 <mariomdomenech@gmail.com>
2021-11-07 23:25:46 -0500

    Created Element
    - Created the main nav-bar element and populated it with its basic values.
    - Set the max height and width of the application to a definitive value to begin working on the smallest viewport.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import React, { Component } from 'react';
+import { AiOutlineMenu, AiOutlineSearch } from 'react-icons/ai';
 
-function App() {
-  return (
-    <div className="container">
-      <h1>HelloS</h1>
-    </div>
-  );
+export default class App extends Component {
+  render () {
+    return (
+      <div className="container">
+        <div className='main-nav-bar'>
+          < AiOutlineMenu className='menuBtn' />
+          < AiOutlineSearch className='searchBtn' />
+
+          <div className='searchbarContainer '>
+            <input type='text' placeholder='Search Quiz...' />
+            < AiOutlineSearch className='searchBtn'/> 
+          </div>
+        </div>
+        
+      </div>
+    );
+  }
 }
 
-export default App;
+// export default App;
