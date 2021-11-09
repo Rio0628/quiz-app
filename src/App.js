d8c08db86c39f43bce30afe68116a6b396483333
Rio0628 <mariomdomenech@gmail.com>
2021-11-08 20:18:39 -0500

    Created Elements
    - Created new iterations of the nav-bar.
    - Created the element of the sidebar for when the small viewport is being shown.

--- src/App.js
+++ src/App.js
@@ -5,20 +5,32 @@ import SavedQuizzesView from './components/SavedQuizzesView';
 
 export default class App extends Component {
   render () {
+  
     return (
       <div className="container">
         <div className='main-nav-bar'>
-          < AiOutlineMenu className='menuBtn' />
+          {/* < AiOutlineMenu className='menuBtn' /> */}
           {/* < AiOutlineSearch className='searchBtn' /> */}
+          
+          {/* <p className='createQuizNavHdng'>Create Quiz</p> */}
+          <p className='quizNavHdng'>Quiz Name</p>
 
-          <div className='searchbarContainer '>
+          {/* <div className='searchbarContainer '>
             <input className='searchbar' type='text' placeholder='Search Quiz...' />
             < AiOutlineSearch className='searchBtn'/> 
-          </div>
+          </div> */}
         </div>
-        
+
+
         <SavedQuizzesView />
         {/* <MainQuizView /> */}
+   
+        <div className='sidebar'>
+          <p className='searchQuiz'>Search Quiz</p>
+          <p className='createQuiz'>Create Quiz</p>
+          <p className='savedQuizzes'>Saved Quizzes</p>
+          <p className='resultQuizzes'>Result Quizzes</p>
+        </div>
 
       </div>
     );
